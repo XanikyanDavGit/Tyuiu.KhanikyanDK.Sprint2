82668072c6ff0ca5227519ea0d86ea70c7061146
xanik <xanikyan00@gmail.com>
2025-09-28 17:47:34 +0500

    Task2

---
+++ Tyuiu.KhanikyanDK.Sprint2.Task2.V4.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.KhanikyanDK.Sprint2.Task2.V4.Lib
+{
+    public class DataService : ISprint2Task2V4
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {   
+            if (x == 3 && (y == 3 || y == 4 || y == 7 || y == 11)) return true;
+            else if (x == 4 && (y == 3 || y == 4 || (y >= 7 && y <= 13))) return true;
+            else if (x == 5 && (y == 14 || (y >= 3 && y <= 7))) return true;
+            else if (x == 6 && (y == 14 || (y >= 5 && y <= 7))) return true;
+            else if (x == 7 && (y >= 5 && y <= 7)) return true;
+            else if (x == 8 && (y >= 5 && y <= 12)) return true;
+            else if (x == 9 && (y >= 3 && y <= 12)) return true;
+            else if (x == 10 && (y >= 7 && y <= 12)) return true;
+            else if (x == 11 && (y == 11 || (y >= 7 && y <= 8))) return true;
+            else if (x == 12 && (y == 11 || (y >= 3 && y <= 8))) return true;
+            else if (x == 13 && (y >= 6 && y <= 8)) return true;
+            else return false;
+        }
+    }
+}
---
+++ Tyuiu.KhanikyanDK.Sprint2.Task2.V4.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KhanikyanDK.Sprint2.Task2.V4.Lib;
+namespace Tyuiu.KhanikyanDK.Sprint2.Task2.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            int x = 3;
+            int y = 4;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
