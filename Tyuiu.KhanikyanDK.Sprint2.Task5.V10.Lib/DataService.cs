26dba1f46d88696840ca7ea8e15c355ccff01aaf
xanik <xanikyan00@gmail.com>
2025-10-02 14:03:56 +0500

    Task5

---
+++ Tyuiu.KhanikyanDK.Sprint2.Task5.V10.Lib/DataService.cs
@@ -0,0 +1,53 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.KhanikyanDK.Sprint2.Task5.V10.Lib
+{
+    public class DataService : ISprint2Task5V10
+    {
+        public string FindDateOfPreviousDay(int g, int m, int n)
+        {
+            string res;
+            switch (n)
+            {
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                case 12:
+                case 13:
+                case 14:
+                case 15:
+                case 16:
+                case 17:
+                case 18:
+                case 19:
+                case 20:
+                case 21:
+                case 22:
+                case 23:
+                case 24:
+                case 25:
+                case 26:
+                case 27:
+                case 28:
+                case 29:
+                case 30:
+                case 31:
+                    res = $"{g}/{m}/{n-1}";
+                    break;
+                case 1:
+                    res = $"{g}/{m-1}/31";
+                    break;
+                default:
+                    res = "n должен быть от 1 до 31";
+                    break;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KhanikyanDK.Sprint2.Task5.V10.Task/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KhanikyanDK.Sprint2.Task5.V10.Lib;
+namespace Tyuiu.KhanikyanDK.Sprint2.Task5.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int g = 2025;
+            int m = 10;
+            int n = 2;
+            string prev = $"{g}/{m}/{n-1}";
+            string exp = "2025/10/1";
+            Assert.AreEqual(exp, prev);
+        }
+    }
+}
