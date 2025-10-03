7df2e00be1bc9c5976c93c17ad8f60e51e738a43
xanik <xanikyan00@gmail.com>
2025-10-03 19:38:05 +0500

    Task6

--- Tyuiu.KhanikyanDK.Sprint2.Task6.V12.Lib/DataService.cs
+++ Tyuiu.KhanikyanDK.Sprint2.Task6.V12.Lib/DataService.cs
@@ -1,7 +1,7 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint2;
 namespace Tyuiu.KhanikyanDK.Sprint2.Task6.V12.Lib
 {
-    public class DataService : ISprint2Task5V10
+    public class DataService : ISprint2Task6V12
     {
         public string FindDateOfPreviousDay(int g, int m, int n)
         {
